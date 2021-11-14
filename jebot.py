d7d233e714bd3770169c46d49b866feef8f2bf75
MrC《》VENOM <77907590+nandhunair1@users.noreply.github.com>
2021-11-14 21:47:30 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -1,17 +1,15 @@
-#    Copyright (c) 2021 Infinity BOTs <https://t.me/Infinity_BOTs>
- 
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
+#                     ----------------------------- @Peterparker6 ---------------------------
+
 
 import os
+import time
 import aiohttp
-from pyrogram import filters, Client
+import ffmpeg
+import logging
+import requests
+from pyrogram import filters, Client, idle
+from youtube_search import YoutubeSearch
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pytube import YouTube
 from youtubesearchpython import VideosSearch
 from sample_config import Config
@@ -27,213 +25,114 @@ Jebot = Client(
    bot_token=Config.TG_BOT_TOKEN,
 )
 
-def yt_search(song):
-    videosSearch = VideosSearch(song, limit=1)
-    result = videosSearch.result()
-    if not result:
-        return False
-    else:
-        video_id = result["result"][0]["id"]
-        url = f"https://youtu.be/{video_id}"
-        return url
+## Extra Fns -------
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
-class AioHttp:
-    @staticmethod
-    async def get_json(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.json()
-
-    @staticmethod
-    async def get_text(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.text()
-
-    @staticmethod
-    async def get_raw(link):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(link) as resp:
-                return await resp.read()
-
- #For private messages        
- #Ignore commands
- #No bots also allowed
-@Jebot.on_message(filters.private & ~filters.bot & ~filters.command("help") & ~filters.command("start") & ~filters.command("s"))  
-#Lets Keep this Simple
-async def song(client, message):
-  # Hope this will fix the args issue
-  # defining args as a array instead of direct defining
-  # also splitting text for correct yt search
-  
-
-    message.chat.id
-    user_id = message.from_user["id"]
-    args = message.text.split(None, 1)
-    args = str(args)
-    # Adding +song for better  searching
-    args = args + " " + "song"
-    #Defined above.. THINK USELESS
-    #args = get_arg(message) + " " + "song"
-
-    #Added while callback... I think Useless    
-    #if args.startswith("/help"):
-        #return ""    
-    status = await message.reply(
-             text="<b>Downloading your song, Plz wait 🥺\n\nMade by @Infinity_BOTs 🇱🇰</b>",
-             disable_web_page_preview=True,
-                        reply_markup=InlineKeyboardMarkup(
-                            [[
-                                    InlineKeyboardButton(
-                                        "Developer", url="https://t.me/ImJanindu")
-                                ]]
-                        ),
-               parse_mode="html",
-        reply_to_message_id=message.message_id
-      )
-    video_link = yt_search(args)
-    if not video_link:
-        await status.edit("<b>Song not found 😑</b>")
-        return ""
-    yt = YouTube(video_link)
-    audio = yt.streams.filter(only_audio=True).first()
-    try:
-        download = audio.download(filename=f"{str(user_id)}")
-    except Exception as ex:
-        await status.edit("<b>Failed to download song 🤕</b>")
-        LOGGER.error(ex)
-        return ""
-    os.rename(download, f"{str(user_id)}.mp3")
-    await Jebot.send_chat_action(message.chat.id, "upload_audio")
-    await Jebot.send_audio(
-        chat_id=message.chat.id,
-        audio=f"{str(user_id)}.mp3",
-        duration=int(yt.length),
-        title=str(yt.title),
-        performer=str(yt.author),
-        reply_to_message_id=message.message_id,
-    )
-    await status.delete()
-    os.remove(f"{str(user_id)}.mp3")    
-    
-    
-    
-@Jebot.on_message(filters.command("s"))
-async def song(client, message):
-    message.chat.id
-    user_id = message.from_user["id"]
-    args = get_arg(message) + " " + "song"
-    if args.startswith(" "):
-        await message.reply("<b>Enter a song name❗\n\nExample: `/s guleba`</b>")
-        return ""
-    status = await message.reply(
-             text="<b>Downloading your song, Plz wait 🥺\n\nMade by @Infinity_BOTs 🇱🇰</b>",
-             disable_web_page_preview=True,
-                        reply_markup=InlineKeyboardMarkup(
-                            [[
-                                    InlineKeyboardButton(
-                                        "Developer", url="https://t.me/ImJanindu")
-                                ]]
-                        ),
-               parse_mode="html",
-        reply_to_message_id=message.message_id
-      )
-    video_link = yt_search(args)
-    if not video_link:
-        await status.edit("<b>Song not found 😑</b>")
-        return ""
-    yt = YouTube(video_link)
-    audio = yt.streams.filter(only_audio=True).first()
-    try:
-        download = audio.download(filename=f"{str(user_id)}")
-    except Exception as ex:
-        await status.edit("<b>Failed to download song 🤕</b>")
-        LOGGER.error(ex)
-        return ""
-    os.rename(download, f"{str(user_id)}.mp3")
-    await Jebot.send_chat_action(message.chat.id, "upload_audio")
-    await Jebot.send_audio(
-        chat_id=message.chat.id,
-        audio=f"{str(user_id)}.mp3",
-        duration=int(yt.length),
-        title=str(yt.title),
-        performer=str(yt.author),
-        reply_to_message_id=message.message_id,
-    )
-    await status.delete()
-    os.remove(f"{str(user_id)}.mp3")
-
-@Jebot.on_message(filters.command("start"))
+## Commands --------
+@Client.on_message(filters.command(['start']))
 async def start(client, message):
-   if message.chat.type == 'private':
-       await Jebot.send_message(
-               chat_id=message.chat.id,
-               text="""<b>Hey There, I'm a Song Downloader Bot
-
-Made by @ImJanindu 🇱🇰
-
-Hit help button to find out more about how to use me</b>""",   
-                            reply_markup=InlineKeyboardMarkup(
-                                [[
-                                        InlineKeyboardButton(
-                                            "Help", callback_data="help"),
-                                        InlineKeyboardButton(
-                                            "Channel", url="https://t.me/Infinity_BOTs")
-                                    ]]
-                            ),        
-            disable_web_page_preview=True,        
-            parse_mode="html",
-            reply_to_message_id=message.message_id
+       await message.reply("👋 𝗛𝗲𝗹𝗹𝗼 𝗕𝗿𝗼\n\n𝐈 𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫[🎶](https://telegra.ph/file/92a1f08c6ca91e0e8c163.mp4)\n\n𝑺𝒆𝒏𝒕 𝒕𝒉𝒆 𝑵𝒂𝒎𝒆 𝒐𝒇 𝒕𝒉𝒆 𝐒𝐨𝐧𝐠 𝒀𝒐𝒖 𝑾𝒂𝒏𝒕... 😍🥰🤗\n<b>Also I Support Inline YouTube Search 😉</b>\n\n𝗝𝘂𝘀𝘁 𝗧𝘆𝗽𝗲 𝗮 𝗦𝗼𝗻𝗴 𝗡𝗮𝗺𝗲\n\n𝐄𝐠. `Believer`",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿', url='https://t.me/Peterparker6'),
+                    InlineKeyboardButton('𝗦𝗼𝘂𝗿𝗰𝗲', url='https://github.com/Avengers105/Music-Bot')
+                ],
+                [
+                    InlineKeyboardButton('Search Inline', switch_inline_query_current_chat='')
+                ]
+            ]
         )
-   else:
+    )
 
-       await Jebot.send_message(
-               chat_id=message.chat.id,
-               text="""<b>Song Downloader Online\n\n</b>""",   
-                            reply_markup=InlineKeyboardMarkup(
-                                [[
-                                        InlineKeyboardButton(
-                                            "Help", callback_data="help")
-                                        
-                                    ]]
-                            ),        
-            disable_web_page_preview=True,        
-            parse_mode="html",
-            reply_to_message_id=message.message_id
-        )
-
-@Jebot.on_message(filters.command("help"))
+@Client.on_message(filters.command(['help']))
 async def help(client, message):
-    if message.chat.type == 'private':   
-        await Jebot.send_message(
-               chat_id=message.chat.id,
-               text="""<b>Send a song name to download song
-
-~ @Infinity_BOTs</b>""",
-            reply_to_message_id=message.message_id
+       await message.reply("<b>Simplest Way😂</b>\n\n<i>How many times have I said that just giving the name of a song is enough.🙄\nDo not expect any other help from me😠</i>\n\n<b>Eg</b> `Vaathi Coming`",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('𝗦𝗼𝘂𝗿𝗰𝗲', url='https://github.com/Avengers105/Music-Bot')
+                ]
+            ]
         )
-    else:
-        await Jebot.send_message(
-               chat_id=message.chat.id,
-               text="<b>Song Downloader Help\n\nEnter a song name❗\n\nExample: `/s guleba`</b>",
-            reply_to_message_id=message.message_id
-        )     
-        
+    )
 
-@Jebot.on_callback_query()
-async def button(Jebot, update):
-      cb_data = update.data
-      if "help" in cb_data:
-        await update.message.delete()
-        await help(Jebot, update.message)
+@Client.on_message(filters.command(['about']))
+async def about(client, message):
+       await message.reply("➪<b>Name</b> : ✫<i>Music Downloader</i>\n➪<b>Developer</b> : ✫[𝘗𝘦𝘵𝘦𝘳 𝘗𝘢𝘳𝘬𝘦𝘳](https://t.me/Peterparker6)\n➪<b>Language</b> : ✫<i>Python3</i>\n➪<b>Server</b> : ✫[𝘏𝘦𝘳𝘰𝘬𝘶](https://heroku.com/)\n➪<b>Source Code</b> : ✫[𝘊𝘭𝘪𝘤𝘬 𝘏𝘦𝘳𝘦](https://github.com/Avengers105/Music-Bot)",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('Search Inline', switch_inline_query_current_chat='')
+                ]
+            ]
+        )
+    )
 
-print(
-    """
-Bot Started!
+@Client.on_message(filters.text)
+def a(client, message):
+    query=message.text
+    print(query)
+    m = message.reply('🔎 𝗦𝗲𝗮𝗿𝗰𝗵𝗶𝗻𝗴 𝘁𝗵𝗲 𝗦𝗼𝗻𝗴...')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
 
-Join @Infinity_BOTs
-"""
-)
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
 
-Jebot.run()
+            views = results[0]["views"]
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('𝐅𝐨𝐮𝐧𝐝 𝐍𝐨𝐭𝐡𝐢𝐧𝐠. 𝐓𝐫𝐲 𝐂𝐡𝐚𝐧𝐠𝐢𝐧𝐠 𝐓𝐡𝐞 𝐒𝐩𝐞𝐥𝐥𝐢𝐧𝐠 𝐀 𝐋𝐢𝐭𝐭𝐥𝐞 😐')
+            return
+    except Exception as e:
+        m.edit(
+            "❎ 𝐹𝑜𝑢𝑛𝑑 𝑁𝑜𝑡ℎ𝑖𝑛𝑔. 𝐒𝐨𝐫𝐫𝐲.\n\n𝖯𝗅𝖾𝖺𝗌𝖾 𝖳𝗋𝗒 𝖠𝗀𝖺𝗂𝗇 𝖮𝗋 𝖲𝖾𝖺𝗋𝖼𝗁 𝖺𝗍 Google.com 𝖥𝗈𝗋 𝖢𝗈𝗋𝗋𝖾𝖼𝗍 𝖲𝗉𝖾𝗅𝗅𝗂𝗇𝗀 𝗈𝖿 𝗍𝗁𝖾 𝙎𝙤𝙣𝙜.\n\nEg.`Believer`"
+        )
+        print(str(e))
+        return
+    m.edit("`Uploading Your Song,Please Wait...`[🎧](https://telegra.ph/file/33e209cb838912e8714c9.mp4)")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep =  f'🎧 𝗧𝗶𝘁𝘁𝗹𝗲 : [{title[:35]}]({link})\n⏳ 𝗗𝘂𝗿𝗮𝘁𝗶𝗼𝗻 : `{duration}`\n👀 𝗩𝗶𝗲𝘄𝘀 : `{views}`\n\n📮 𝗕𝘆: {message.from_user.mention()}\n📤 𝗕𝘆 : @MusicDownloadv2bot'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=str(info_dict["uploader"]), thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit('𝙁𝙖𝙞𝙡𝙚𝙙\n\n`Plesase Try Again Later`')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
